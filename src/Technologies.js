43dc4ac27ee050101c611546b7f7077bcddb4c63
zak <zakvova@i.ua>
2024-03-13 10:24:09 +0200

    first commit

---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Technogolies = () => {
+  return (
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+      </ul>
+  )
+}
+
+export default Technogolies
