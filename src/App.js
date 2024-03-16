47d44a5f1499dfd330bbe70d1a0d7f49d0bf921e
zak <zakvova@i.ua>
2024-03-16 10:31:20 +0200

    Урок 11 Default Опасность Плагин Auto Import

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import Technogolies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-     
-     tes
-      <Technogolies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="samuraiLogo.png"></img>
+      </header>
+      <nav className="nav">
+        <div><a href="#">Profile</a></div>
+        <div><a href="#">Messages</a></div>
+        <div><a href="#">News</a></div>
+        <div><a href="#">Music</a></div>
+        <div><a href="#">Settings</a></div>
+      </nav>
+      <div className="content">
+        <div className="contentTopImg">
+        </div>
+        <div>Ava + description</div>
+        <div>My posts
+          <div>New post</div>
+        </div>
+        <div>
+          <div>Post1</div>
+          <div>Post2</div>
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
