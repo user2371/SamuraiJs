3400a948fd1618a2c75d1f4ac29b393d15469b21
zak <zakvova@i.ua>
2024-03-25 13:33:50 +0200

    Урок 19 Route, Browser-Router, Маршрутизация

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from '../node_modules/react-router-dom/dist/index';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="appWrapper">
       <Header />
       <Navbar />
       <div className="appWrapperContent">
-        <Profile />
-        <Dialogs />
+        <Routes>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/dialogs" element={<Dialogs/>}></Route>
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import Styles from "./Navbar.module.css"
 const Navbar = (props) => {
     return (
         <nav className={Styles.nav}>
-        <div><a href="#">Profile</a></div>
-        <div><a href="#">Messages</a></div>
+        <div><a href="/profile">Profile</a></div>
+        <div><a href="/dialogs">Messages</a></div>
         <div><a href="#">News</a></div>
         <div><a href="#">Music</a></div>
         <div><a href="#">Settings</a></div>
