0ee3004ae699a53cf5561043677ab6e7cf9fb86b
zak <zakvova@i.ua>
2024-03-26 20:53:59 +0200

    Урок 21 Верстаем страницу диалого

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div className={`${styles.red} ${styles.content}`}>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogItems}>
+                <div className={styles.dialogItem }>Dimych</div>
+                <div className={styles.dialogItem}>Andrey</div>
+                <div className={styles.dialogItem + " " + styles.active}>Sveta</div>
+                <div className={styles.dialogItem}>Sasha</div>
+                <div className={styles.dialogItem}>Victor</div>
+                <div className={styles.dialogItem}>Valera</div>
+            </div>
+            <div className={styles.dialogMessages}>
+                <div className={styles.dialogMessage}>Hi</div>
+                <div className={styles.dialogMessage}>What's up</div>
+                <div className={styles.dialogMessage}>Good bye</div>
+            </div>
         </div>
     )
 }
