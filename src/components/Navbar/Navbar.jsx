af7196205481e389aa3c9e6c976f928b7ba6e584
zak <zakvova@i.ua>
2024-03-26 15:32:02 +0200

    Урок 20 (NavLink)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css"
+import { NavLink } from '../../../node_modules/react-router-dom/dist/index';
 
 const Navbar = (props) => {
     return (
-        <nav className={Styles.nav}>
-        <div><a href="/profile">Profile</a></div>
-        <div><a href="/dialogs">Messages</a></div>
+        <nav className={styles.nav}>
+        <div><NavLink to="/profile" className={({isActive}) => isActive ? styles.active : undefined}>Profile</NavLink></div>
+        <div><NavLink to="/dialogs" className={({isActive}) => isActive ? styles.active : undefined}>Messages</NavLink></div>
         <div><a href="#">News</a></div>
         <div><a href="#">Music</a></div>
         <div><a href="#">Settings</a></div>
