0908bc68a8883d01fac4ff6c95f0291f9fbc805d
zakv-v <zakvova@i.ua>
2025-06-08 20:59:37 +0300

    Урок 53 классовый компонент (extends React.Component)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,50 @@
 import React from 'react';
-import styles from './Users.module.css';
+import styles from './Users.module.css'
 import axios from '../../../node_modules/axios/index.js';
 import userIcon from "../../assets/images/userIcon.png";
 
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then((response) => {
-                    props.setUsers(response.data.items)
-                })
-        }
+class UsersC extends React.Component {
+    constructor(props) {
+        super(props)
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((response) => {
+                this.props.setUsers(response.data.items) // тут this.props
+            })
     }
-    return (
-        <div className={styles.users}>
-            <button onClick={getUsers}>GetUsers</button>
-            {props.users.map((user) => {
-                return (
-                    <div className={styles.user} key={user.id}>
-                        <div className={styles.userLeftSide}>
-                            <div className={styles.userAvatar}><img src={user.photos.small !== null ? user.photos.small : userIcon} /></div>
-                            <div className={styles.followBtnContainer}>
-                                {user.isFollowed
-                                    ? <button onClick={() => { props.unFollow(user.id) }}>Unfollow</button>
-                                    : <button onClick={() => { props.follow(user.id) }}>follow</button>
-                                }
-                            </div>
-                        </div>
 
-                        <div className={styles.userDescription}>
-                            <div>
-                                <h3>{user.name}</h3>
-                                <p>{user.status}</p>
+    render() {
+        return (
+            <div className={styles.users}>
+                {this.props.users.map((user) => {
+                    return (
+                        <div className={styles.user} key={user.id}>
+                            <div className={styles.userLeftSide}>
+                                <div className={styles.userAvatar}><img src={user.photos.small !== null ? user.photos.small : userIcon} /></div>
+                                <div className={styles.followBtnContainer}>
+                                    {user.isFollowed
+                                        ? <button className={styles.redBtn} onClick={() => { this.props.unFollow(user.id) }}>Unfollow</button>
+                                        : <button className={styles.greenBtn} onClick={() => { this.props.follow(user.id) }}>follow</button>
+                                    }
+                                </div>
                             </div>
-                            <div className={styles.location}>
-                                <p>{"user.location.country"}</p>
-                                <p>{"user.location.city"}</p>
+
+                            <div className={styles.userDescription}>
+                                <div>
+                                    <h3>{user.name}</h3>
+                                    <p>{user.status}</p>
+                                </div>
+                                <div className={styles.location}>
+                                    <p>{"user.location.country"}</p>
+                                    <p>{"user.location.city"}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div >
-    )
+                    )
+                })}
+            </div >
+        )
+    }
 }
 
-
-
-
-export default Users;
-
-
+export default UsersC;
