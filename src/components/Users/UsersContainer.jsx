8c2c91b1cdaf7468802b6c9c154a76f854bae4fd
zakv-v <zakvova@i.ua>
2025-06-15 20:35:11 +0300

    Урок 56 Презентационная и Контейнерная компоненты

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,9 +1,108 @@
 import React from 'react';
 import { connect } from '../../../node_modules/react-redux/dist/react-redux';
-import Users from './Users';
 import { followAC, setCurrentPageAC, setTotalUsersCount, setUsersAC, unFollowAC } from '../../redux/users-reducer';
+import UsersPresentational from './UsersPresentational';
+import axios from 'axios';
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+    onFirstPageDoubleArrowClick = () => {
+        this.props.setCurrentPage(1);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${1}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+
+    onLastPageDoubleArrowClick = () => {
+        let lastPage = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        this.props.setCurrentPage(lastPage);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${lastPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+    onNumberInputChange = (page) => {
+        if (typeof (page) !== "number") {
+            page = +page
+        }
+        if (page < 1 || page === null || page > Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
+            return
+        } else {
+
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+                .then((response) => {
+                    this.props.setUsers(response.data.items);
+                    this.props.setCurrentPage(page);
+                })
+        }
+    }
+
+    onLeftArrowClick = () => {
+        let previousPage = this.props.currentPage - 1
+        if (previousPage < 1) {
+            return
+        }
+        this.props.setCurrentPage(previousPage);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${previousPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+    onRightArrowClick = () => {
+        let previousPage = this.props.currentPage + 1
+        if (previousPage > Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
+            return
+        }
+        this.props.setCurrentPage(previousPage);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${previousPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+    
 
 
+    render() {
+        return <UsersPresentational            
+            onFirstPageDoubleArrowClick={this.onFirstPageDoubleArrowClick}
+            onLeftArrowClick={this.onLeftArrowClick}
+            onPageChanged={this.onPageChanged}
+            currentPage={this.props.currentPage}
+            onRightArrowClick={this.onRightArrowClick}
+            onLastPageDoubleArrowClick={this.onLastPageDoubleArrowClick}
+            onNumberInputChange={this.onNumberInputChange}
+            users={this.props.users}
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            follow={this.props.follow}
+            unFollow={this.props.unFollow}
+
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
---
+++ src/components/Users/UsersPresentational.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import UsersPagination from './UsersPagination/UsersPagination';
+import styles from './Users.module.css'
+import userIcon from "../../assets/images/userIcon.png";
+
+let UsersPresentational = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    let createPages = (pages, pagesCount, currentPage) => {
+            if (pagesCount > 5) {
+                if (currentPage > 2) {
+                    for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+                        pages.push(i)
+                        if (i == pagesCount) break
+                    }
+                }
+                else {
+                    for (let i = 1; i <= 5; i++) {
+                        pages.push(i)
+                        if (i == pagesCount) break
+                    }
+                }
+            } else {
+                for (let i = 1; i <= pagesCount; i++) {
+                    pages.push(i)
+                }
+            }
+        }
+    createPages(pages, pagesCount, props.currentPage);
+
+    return (
+        <div className={styles.users}>
+            <UsersPagination
+                pages={pages}
+                onFirstPageDoubleArrowClick={props.onFirstPageDoubleArrowClick}
+                onLeftArrowClick={props.onLeftArrowClick}
+                onPageChanged={props.onPageChanged}
+                currentPage={props.currentPage}
+                onRightArrowClick={props.onRightArrowClick}
+                onLastPageDoubleArrowClick={props.onLastPageDoubleArrowClick}
+                onNumberInputChange={props.onNumberInputChange}
+                pagesCount={pagesCount}
+            >
+            </UsersPagination>
+            {props.users.map((user) => {
+                return (
+                    <div className={styles.user} key={user.id}>
+                        <div className={styles.userLeftSide}>
+                            <div className={styles.userAvatar}><img src={user.photos.small !== null ? user.photos.small : userIcon} /></div>
+                            <div className={styles.followBtnContainer}>
+                                {user.isFollowed
+                                    ? <button className={styles.redBtn} onClick={() => { props.unFollow(user.id) }}>Unfollow</button>
+                                    : <button className={styles.greenBtn} onClick={() => { props.follow(user.id) }}>follow</button>
+                                }
+                            </div>
+                        </div>
+
+                        <div className={styles.userDescription}>
+                            <div>
+                                <h3>{user.name}</h3>
+                                <p className={styles.userStatus}>{user.status}</p>
+                            </div>
+                            <div className={styles.location}>
+                                <p>{"user.location.country"}</p>
+                                <p>{"user.location.city"}</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div >
+    )
+}
+    
+export default UsersPresentational;
